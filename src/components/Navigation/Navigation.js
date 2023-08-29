80ded99870ac2ce92af85eb881692d20af2dd51d
Mark Zaitsev <mark.zaytsev@yandex.ru>
2023-08-29 03:03:17 +0300

    done Header styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,29 +6,35 @@ import accIcon from "../../images/account-icon.svg";
 
 export function Navigation() {
     
+const logedIn = true;
 
     return (
-            // {!logedIn && (
-            //     <div className="navigation__no-auth">
-            //         <Link className="navigation__button-signup">Регистрация</Link>
-            //         <Link className="navigation__button-sigin">Войти</Link>
-            //     </div>
-            // )}
+        <>
+            {!logedIn && (
+                <div className="navigation__notauth">
+                    <Link className="navigation__button-signup">Регистрация</Link>
+                    <Link className="navigation__button-signin">Войти</Link>
+                </div>
+            )}
 
-            // {logedIn && (
-            //     
+            {logedIn && (
                 <div className="navigation__auth">
-                    <Link className="navigation__">Фильмы</Link>
-                    <Link className="navigation__">Сохранённые фильмы</Link>
+                    <div className="navigation__container-auth">
+                        <div className="navigation__films">
+                            <Link className="navigation__all-films">Фильмы</Link>
+                            <Link className="navigation__my-films">Сохранённые фильмы</Link>
+                        </div>
 
-                    <Link className="navigation__account-button">
-                        <img className="navigation__account-icon" alt="иконка аккаунта" src={accIcon}/>
-                        <p className="navigation__account-text">Аккаунт</p>
-                    </Link>
-
+                        <Link className="navigation__account-button">
+                            <p className="navigation__account-text">Аккаунт</p>
+                            <div className="navigation__account-icon" alt="иконка аккаунта"></div>
+                        </Link>
+                    </div>
                     <Burger />
                 </div>
-            // )}
+            )}
+        </>
+            
         
        
     )
